fb18bf2181e99e8072bef52b3c7cc37a5e66de21
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2025-11-26 16:58:24 -0500

    Implemented a check for username/password

--- makerspace-frontend/src/Login.tsx
+++ makerspace-frontend/src/Login.tsx
@@ -1,9 +1,8 @@
-import {FormEventHandler, type SyntheticEvent, useState} from "react";
+import {type SyntheticEvent, useState} from "react";
 import "./Login.css";
 import quinnipacLogo from './assets/Logo.svg'
 import { useNavigate } from "react-router-dom";
 import {Button, Form} from "react-bootstrap";
-import * as events from "node:events";
 
 function Login({setIsLoggedIn}: { setIsLoggedIn: () => void }) {
     const [errorMessage, setErrorMessage] = useState("");
@@ -11,9 +10,28 @@ function Login({setIsLoggedIn}: { setIsLoggedIn: () => void }) {
 
     function handleLogin(event: SyntheticEvent<HTMLFormElement>): void {
         event.preventDefault()
-        setIsLoggedIn();
-        navigate('/home');
+        const form: HTMLFormElement = event.currentTarget;
+        if (validUsernameAndPassword(form.username.value, form.password.value)) {
+            setIsLoggedIn();
+            navigate('/home');
+        }
+        else {
+            setErrorMessage("Invalid username or password.");
+        }
+
     }
+
+    // Placeholder, will be revamped when implementing the backend
+    function validUsernameAndPassword(username: string, password: string): boolean {
+        for (let user of users) {
+            if (user.username == username && user.password == password) {
+                return true
+            }
+        }
+        return false
+    }
+
+    const users: {username: string, password: string}[] = [{username: "user@qu.edu", password: "password"}]
 
     return (
         <div className="login">
